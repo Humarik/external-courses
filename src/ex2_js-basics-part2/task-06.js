1399b618fef3f9bfa774a2cef95926c1d2a2e970
Hurma <piplap62@gmail.com>
2019-09-23 12:05:17 +0300

    added js-basics-part2 task-06
    *Added function that checks number and shows prime number or not

---
+++ src/ex2_js-basics-part2/task-06.js
@@ -0,0 +1,23 @@
+function checkPrime(number) {
+    let count = 0;
+    let result = '';
+    if (number > 1000 || number < 2) {
+        return 'Данные неверны';
+    }
+
+    for (let i = 1; i <= number; i++) {
+        if (number % i === 0 || number === 2) {
+            if (i <= number) {
+                count++;
+            }
+        }
+    }
+
+    if (count > 2) {
+        result = 'Число ' + number + ' - составное число';
+    } else if (count === 2) {
+        result = 'Число ' + number + ' - простое число';
+    }
+    return result;
+}
+module.exports = checkPrime;
