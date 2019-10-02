dfd6d88893aeb919e72faaef1179dcfd1b7d6d0b
Hurma <piplap62@gmail.com>
2019-10-02 23:37:16 +0300

    Add task-06
    The deepCopyObj is function that makes deep copy obj with help recursion

---
+++ src/ex3_js-objects-part1/task-06.js
@@ -0,0 +1,19 @@
+function deepCopyObj(data) {
+    const copyObj = {};
+    for (let key in data) {
+        if (data[key] instanceof Object) {
+            if (data[key] instanceof Array){
+                copyObj[key] = [];
+                for (let i = 0; i < data[key].length;i++){
+                    copyObj[key].push((data[key][i]));
+                }
+            }else{
+                copyObj[key] = deepCopyObj(data[key]);
+            }
+        } else {
+            copyObj[key] = data[key];
+        }
+    }
+    return copyObj;
+}
+module.exports = deepCopyObj;
