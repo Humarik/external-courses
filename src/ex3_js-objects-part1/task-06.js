916866f93fcef3bd43582a5f055191ddf35349d0
Hurma <piplap62@gmail.com>
2019-10-07 11:38:46 +0300

    Fix task-06
    Function deepCopyObj has been changed because I noticed mistake
    in my code but last test passed successfully.
    Any way I needed to fix my homework. I found article where says that
    I can use cycle 'for in' for array too. After this my code's became less

--- src/ex3_js-objects-part1/task-06.js
+++ src/ex3_js-objects-part1/task-06.js
@@ -1,15 +1,8 @@
 function deepCopyObj(data) {
-    const copyObj = {};
+    let copyObj = data instanceof Array ? [] : {};
     for (let key in data) {
         if (data[key] instanceof Object) {
-            if (data[key] instanceof Array){
-                copyObj[key] = [];
-                for (let i = 0; i < data[key].length;i++){
-                    copyObj[key].push((deepCopyObj(data[key][i])));
-                }
-            }else{
-                copyObj[key] = deepCopyObj(data[key]);
-            }
+            copyObj[key] = deepCopyObj(data[key]);
         } else {
             copyObj[key] = data[key];
         }
