c22260fb99b2f1ae22bd906343e48c67bab2beae
Hurma <piplap62@gmail.com>
2019-11-26 01:22:44 +0300

    All fix both tasks

--- src/ex12_js_dom/task-01.js
+++ src/ex12_js_dom/task-01.js
@@ -1,43 +1,46 @@
-const array = ['asset/squirrel.jpg', 'asset/squirrel-first.jpg', 'asset/mount.jpg', 'asset/flowers.jpg', 'asset/field.jpg'];
+const images = ['asset/squirrel.jpg', 'asset/squirrel-first.jpg', 'asset/mount.jpg', 'asset/flowers.jpg', 'asset/field.jpg'];
+
 let firstImg = document.querySelector('.first-img');
-firstImg.setAttribute('src', array[0]);
+firstImg.setAttribute('src', images[0]);
+
 let secondImg = document.querySelector('.second-img');
-secondImg.setAttribute('src', array[1]);
-const imgBox = document.querySelector('#second-img-wrapper');
+secondImg.setAttribute('src', images[1]);
+
 const slider = document.querySelector('.slider-wrapper');
-slider.addEventListener('click', initImg);
 
 let next = 0;
 let prev = 1;
 
-function counter(target) {
-    if (target === 'next-btn') {
-        next = next >= array.length - 1 ? next = 0 : next += 1;
-        prev = prev >= array.length - 1 ? prev = 0 : prev += 1;
-    } else if (target === 'prev-btn') {
-        next = next === 0 ? next = array.length - 1 : next -= 1;
-        prev = prev === 0 ? prev = array.length - 1 : prev -= 1;
+function counter(className) {
+    if (className === 'next-btn btn-slider') {
+        next = next >= images.length - 1 ? next = 0 : next += 1;
+        prev = prev >= images.length - 1 ? prev = 0 : prev += 1;
+    } else if (className === 'prev-btn btn-slider') {
+        next = next === 0 ? next = images.length - 1 : next -= 1;
+        prev = prev === 0 ? prev = images.length - 1 : prev -= 1;
     }
 }
 
 function initImg(e) {
-    if (e.target.id === 'next-btn' || e.target.id === 'prev-btn') {
-        firstImg.classList.remove('opacity');
-        let image = firstImg;
-        let newImage = image.cloneNode(false);
-        image.parentNode.replaceChild(newImage, image);
-        firstImg = newImage;
+    const target = e.target;
+    if (!target.classList.contains('prev-btn') && !target.classList.contains('next-btn')) return;
+    firstImg.classList.remove('opacity');
+    let image = firstImg;
+    let newImage = image.cloneNode(false);
+    image.parentNode.replaceChild(newImage, image);
+    firstImg = newImage;
 
-        secondImg.classList.remove('opacity');
-        image = secondImg;
-        newImage = image.cloneNode(false);
-        image.parentNode.replaceChild(newImage, image);
-        secondImg = newImage;
+    secondImg.classList.remove('opacity');
+    image = secondImg;
+    newImage = image.cloneNode(false);
+    image.parentNode.replaceChild(newImage, image);
+    secondImg = newImage;
 
-        counter(e.target.id);
-        firstImg.classList.add('opacity');
-        secondImg.classList.add('opacity');
-        firstImg.setAttribute('src', array[next]);
-        secondImg.setAttribute('src', array[prev]);
-    }
+    counter(target.className);
+    firstImg.classList.add('opacity');
+    secondImg.classList.add('opacity');
+    firstImg.setAttribute('src', images[next]);
+    secondImg.setAttribute('src', images[prev]);
 }
+
+slider.addEventListener('click', initImg);
--- src/ex12_js_dom/task-02.js
+++ src/ex12_js_dom/task-02.js
@@ -1,10 +1,23 @@
 const userBlock = document.querySelector('.user-container-wrapper');
 const header = document.querySelector('.header');
+const titles = ['My account', 'My tasks', 'Log out'];
+
+function createLi (text) {
+    const a = document.createElement('a');
+    a.className = 'link';
+    a.innerHTML = text;
+    const li = document.createElement('li');
+    li.className = 'item';
+    li.style.padding = '5px';
+    li.style.margin = '0';
+    li.appendChild(a);
+    return li;
+}
 
 userBlock.addEventListener('click', showUserMenu);
-function showUserMenu(e){
+function showUserMenu (e){
     const arrowUserBtn = document.querySelector('.line-btn-off');
-    if (!e.currentTarget.classList.contains('on')){
+    if (!e.currentTarget.classList.contains('on')) {
         e.currentTarget.classList.toggle('on');
         const ul = document.createElement('ul');
         const div = document.createElement('div');
@@ -13,28 +26,10 @@ function showUserMenu(e){
         nav.style.background = '#0079bf';
         nav.style.height = '100%';
 
-        for (let i = 0; i < 3; i++) {
-            const a = document.createElement('a');
-            a.className = 'link';
-            const li = document.createElement('li');
-            li.className = 'item';
-            li.style.padding = '5px';
-            li.style.margin = '0px 0px 0px 0px';
-            li.appendChild(a);
-            switch (i) {
-                case 0:
-                    a.innerHTML = 'My account';
-                    break;
-                case 1:
-                    a.innerHTML = 'My tasks';
-                    break;
-                case 2:
-                    a.innerHTML = 'Log out';
-                    break;
-                default:
-            }
-            ul.appendChild(li);
-        }
+        titles.reduce((acc, currentValue) => {
+            acc = createLi(currentValue);
+            return ul.appendChild(acc);
+        }, ul);
 
         div.className = 'menu-wrapper';
         div.style.position = 'absolute';
@@ -49,7 +44,7 @@ function showUserMenu(e){
         div.appendChild(nav);
         nav.appendChild(ul);
         header.appendChild(div);
-    } else{
+    } else {
         e.currentTarget.classList.toggle('on');
         arrowUserBtn.classList.toggle('line-btn-on');
         e.currentTarget.parentNode.querySelector('.menu-wrapper').remove();
